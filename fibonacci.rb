7df2f86c819106b63197bf7aa43bd2dd98c60f9e
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-07-23 17:56:32 +0530

    add fibonacci implementation through iteration

--- fibonacci.rb
+++ fibonacci.rb
@@ -3,9 +3,25 @@ def fib_rec(number)
     return number
   end
 
-  fib_rec(number -1) + fib_rec(number -2)
+  fib_rec(number - 1) + fib_rec(number - 2)
 end
 
 12.times do |i|
-  puts fib_rec(i)
+  p fib_rec(i)
+end
+
+
+def fib(n)
+  if n < 2
+    return n
+  end
+  fib_arr = [0, 1]
+  n.times do
+    fib_arr.push(fib_arr.last + fib_arr[-2] )
+  end
+  fib_arr
+end
+
+12.times do |i|
+  p fib(i)
 end
